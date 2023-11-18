cc83c0586ce0f83544b6e3a348c2f3630171d537
Livsy90 <artmir@live.ru>
2023-11-18 15:48:34 +0400

    Initial Commit

---
+++ MulberryChat/MulberryChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MulberryChatApp.swift
+//  MulberryChat
+//
+//  Created by Livsy on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MulberryChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
